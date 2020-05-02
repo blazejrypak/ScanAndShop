5295cb94caca796937b592dd4c579321ee4ca62c
Blazej <rypak.b@gmail.com>
2020-05-02 17:02:47 +0200

    Added translations[en, sk], fixed issue with adding nameless item to shopping list

--- src/screens/ScannerScreen.js
+++ src/screens/ScannerScreen.js
@@ -3,12 +3,13 @@ import * as React from "react";
 import ScanBarCode from "../components/ScanBarCode";
 import {addTrolleyItem} from "../actions";
 import {DEFAUL_COUNT, DEFAUL_PRICE} from "../constants/constants";
+import {strings} from "../locales/i18n";
 
 function ScannerScreen({ trolley, jwt, dispatch, navigation }) {
   function onBarCodeScanned(type, data) {
     console.log("[BAR CODE SCANNED] type: ", type, " data: ", data);
     dispatch(addTrolleyItem(data.toString(), DEFAUL_PRICE, DEFAUL_COUNT, data.toString(), trolley.trolleyId, jwt));
-    navigation.navigate('Trolley');
+    navigation.navigate(strings('ShoppingContainer.shopping_cart' ));
   }
 
   return (
